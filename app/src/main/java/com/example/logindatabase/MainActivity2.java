618d0d619187a51f7e731cf33727edbcbecaed60
charlieyang1986 <cyang21@mail.greenriver.edu>
2021-02-14 15:08:41 -0800

    MainActivity2 version control test

--- app/src/main/java/com/example/logindatabase/MainActivity2.java
+++ app/src/main/java/com/example/logindatabase/MainActivity2.java
@@ -3,9 +3,10 @@ package com.example.logindatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+      private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
