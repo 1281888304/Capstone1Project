4cf62e311d54b6f4c14feb4073bf138bd29eebae
pits96 <59748633+pits96@users.noreply.github.com>
2021-02-24 11:59:12 -0800

    Sprint3 admin

--- login/index.php
+++ login/index.php
@@ -13,11 +13,12 @@ require('checkLogin.php');
 <head>
     <meta charset="UTF-8">
     <title>Admin Page</title>
+    <link rel="icon" type="image" href="images/ethenlogo.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-auth.js"></script>
     <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-database.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-analytics.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-auth.js"></script>
     <script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-firestore.js"></script>
 
 </head>
@@ -36,11 +37,60 @@ require('checkLogin.php');
     <th>Company</th>
     <th>Item</th>
     <th>Quantity</th>
+    <th>Address</th>
     </tr>
     </thead>
-</table>
+</table><br>
+    <h2>Add Order</h2>
 </div>
-
+<div class="container">
+    <div class="mb-5 row">
+        <label for="company" class="col-sm-2 col-form-label">Company</label>
+        <div class="col-sm-5">
+            <input type="text" class="form-control-sm" id="company">
+        </div>
+    </div>
+    <div class="mb-5 row">
+        <label for="item" class="col-sm-2 col-form-label">Item</label>
+        <div class="col-sm-5">
+            <select class="form-select" id="item">
+                <option>----</option>
+                <option value="Tea extract">Tea extract</option>
+                <option value="Orange Syrup">Orange Syrup</option>
+                <option value="Passion Fruit Syrup">Passion Fruit</option>
+                <option value="Honeydew Syrup">Honeydew Syrup</option>
+                <option value="White Peach Popping">White Peach Popping</option>
+            </select>
+        </div>
+    </div>
+    <div class="mb-5 row">
+        <label for="quantity" class="col-sm-2 col-form-label">Quantity</label>
+        <div class="col-sm-5">
+            <input type="text" class="form-control-sm" id="quantity">
+        </div>
+    </div>
+    <div class="mb-5 row">
+        <label for="address" class="col-sm-2 col-form-label">Address</label>
+        <div class="col-sm-5">
+            <input type="text" class="form-control-sm" id="address">
+        </div>
+    </div>
+    <button type="submit" class="btn-primary" id="addOrder">Add</button>
+    <button type="submit" class="btn-primary" id="update">Update</button>
+<!--    <form class="form-group">-->
+<!--        <label for="company">Company</label>-->
+<!--        <input type="text" class="form-check-input" id="company"><br>-->
+<!--        <label for="item" >Item</label>-->
+<!--        <select class="form-select" id="item">-->
+<!--            <option>----</option>-->
+<!--            <option value="Tea extract">Tea extract</option>-->
+<!--            <option value="Orange Syrup">Orange Syrup</option>-->
+<!--            <option value="Passion Fruit Syrup">Passion Fruit</option>-->
+<!--            <option value="Honey Dew Syrup">Honey Dew Syrup</option>-->
+<!--            <option value="White Peach Popping">White Peach Popping</option>-->
+<!--        </select>-->
+<!--    </form>-->
+</div>
 <script src="scripts/firebase.js"></script>
 <script src="//code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
--- login/scripts/firebase.js
+++ login/scripts/firebase.js
@@ -14,32 +14,100 @@ firebase.analytics();
 // var admin = require("firebase-admin");
 
 // Get a database reference to our posts
+var Objcompany = document.getElementById("company");
+var Objitem =document.getElementById("item");
+var Objquantity = document.getElementById("quantity");
+var Objaddress = document.getElementById("address");
+var num=0;
+var submitbtn = document.getElementById("addOrder")
+var updatebtn = document.getElementById("update");
 var db = firebase.database();
 var ref = db.ref("orders");
 const ordertable = document.getElementById("ordersTable")
 let tbody = document.createElement("tbody");
 // Attach an asynchronous callback to read the data at our posts reference
-ref.on("value", function(snapshot) {
+ref.once("value", function(snapshot) {
     console.log(snapshot.val());
     let ss = snapshot.val();
-    for (let i = 0; i < snapshot.val().length; i++) {
+    // for (let i = 0; i < snapshot.val().length; i++) {
+    Object.keys(ss).forEach(function (order){
         let tr = document.createElement("tr");
         let ordernum = document.createElement("th");
         let company = document.createElement("td");
         let item = document.createElement("td");
         let quantity = document.createElement("td");
-        ordernum.innerHTML = ss[i].order_num;
-        company.innerHTML = ss[i].company;
-        item.innerHTML = ss[i].item;
-        quantity.innerHTML = ss[i].quantity;
+        let address = document.createElement("td");
+        let editbtn = document.createElement('button');
+        let delbtn = document.createElement('button');
+        editbtn.innerHTML="Edit";
+        delbtn.innerHTML = "Delete";
+        delbtn.className = 'btn btn-primary';
+        editbtn.className='btn btn-primary';
+        var key = ss[order].order_num;
+        editbtn.value=key;
+        delbtn.value = key;
+        editbtn.addEventListener("click", function (){editOrder(editbtn)});
+        delbtn.addEventListener("click",function(){delOrder(delbtn)});
+        ordernum.innerHTML = ss[order].order_num;
+        company.innerHTML = ss[order].company;
+        item.innerHTML = ss[order].item;
+        quantity.innerHTML = ss[order].quantity;
+        address.innerHTML=ss[order].address;
         tr.append(ordernum);
         tr.append(company);
         tr.append(item);
         tr.append(quantity);
+        tr.append(address);
+        tr.append(editbtn);
+        tr.append(delbtn);
         tbody.append(tr);
-
-    }
+    num++;
+    });
     ordertable.append(tbody);
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
+submitbtn.addEventListener("click",addOrder)
+function addOrder(){
+    console.log(num);
+    var newOrder = ref.child(num);
+    newOrder.set({
+        "order_num": num+1,
+        "company": document.getElementById("company").value,
+        "item": document.getElementById("item").value,
+        "quantity": document.getElementById("quantity").value,
+        "address": document.getElementById("address").value,
+    })
+    console.log(newOrder.key);
+    console.log(document.getElementById("item").value);
+    location.reload(true);
+}
+function editOrder(order){
+    var ordernum = order.value;
+    console.log(ordernum);
+    ref.child(ordernum-1).once('value').then(function(snapshot){
+        console.log(snapshot.val());
+        editsnap = snapshot.val();
+
+        Objcompany.value = editsnap.company;
+        Objitem.value = editsnap.item;
+        Objquantity.value = editsnap.quantity;
+        Objaddress.value = editsnap.address;
+        updatebtn.addEventListener("click",function(){
+           ref.child(ordernum-1).update({'order_num':ordernum, 'company': Objcompany.value,
+           'item':Objitem.value, 'quantity':Objquantity.value,'address':Objaddress.value});
+            location.reload();
+        });
+
+    })
+
+
+}
+function delOrder(order){
+    var confirmation = confirm("Are you sure you want to delete this order?");
+    if(confirmation){
+    var ordernum = order.value;
+    ref.child(ordernum-1).remove();
+    }
+    location.reload(true);
+}
