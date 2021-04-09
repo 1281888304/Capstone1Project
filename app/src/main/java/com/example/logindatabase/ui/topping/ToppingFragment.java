5051981e034000aa3a3cfcb0d8590d5e9b71f504
QINGHANG ZHANG <Qzhang9@mail.greenriver.edu>
2021-04-09 15:06:35 -0700

    Shopping requirement is done

--- app/src/main/java/com/example/logindatabase/ui/topping/ToppingFragment.java
+++ app/src/main/java/com/example/logindatabase/ui/topping/ToppingFragment.java
@@ -14,9 +14,44 @@ import android.view.ViewGroup;
 
 import com.example.logindatabase.R;
 
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class ToppingFragment extends Fragment {
 
     private ToppingViewModel mViewModel;
+    ArrayList<Toppings> toppingsList;
+    private DatabaseReference myRef;
+    private RecyclerView recyclerView;
+    private EditText searchTopping;
+    //adapter
+    private ToppingRecylerAdapter toppingRecylerAdapter;
+
+    private Context mcontext;
 
     public static ToppingFragment newInstance() {
         return new ToppingFragment();
@@ -25,7 +60,109 @@ public class ToppingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.topping_fragment, container, false);
+        View view= inflater.inflate(R.layout.topping_fragment, container, false);
+
+        //code here
+
+        recyclerView=view.findViewById(R.id.recylerView);
+        searchTopping=view.findViewById(R.id.inputSearchTopping);
+        //variables
+
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        //firebase
+        myRef= FirebaseDatabase.getInstance().getReference();
+
+
+        //clear data
+        clearAll();
+
+        //get data model
+        GetDataFromFirebase("");
+
+        //search for data
+        searchTopping.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+
+        return view;
+    }
+
+    //helper function
+    private void filter(String text) {
+
+        ArrayList<Toppings> filterList=new ArrayList<>();
+        for(Toppings item : toppingsList){
+            if(item.getToppingName().toLowerCase().contains(text.toLowerCase())){
+                filterList.add(item);
+            }
+        }
+        toppingRecylerAdapter.filteredList(filterList);
+    }
+
+    private void GetDataFromFirebase(String data) {
+
+        Query query=myRef.child("Product").child("Topping");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                clearAll();
+                for(DataSnapshot snapshot :dataSnapshot.getChildren()){
+                    Toppings toppings=new Toppings();
+
+                    toppings.setToppingImage(snapshot.child("toppingImage").getValue().toString());
+                    toppings.setToppingName(snapshot.child("toppingName").getValue().toString());
+                    toppings.setToppingPrice(snapshot.child("toppingPrice").getValue().toString());
+                    toppings.setToppingNum(snapshot.child("toppingNum").getValue().toString());
+                    toppings.setToppingTitle(snapshot.child("toppingTitle").getValue().toString());
+
+                    //add to list
+                    toppingsList.add(toppings);
+                }
+
+                toppingRecylerAdapter = new ToppingRecylerAdapter(getContext(),toppingsList);
+                recyclerView.setAdapter(toppingRecylerAdapter);
+                toppingRecylerAdapter.notifyDataSetChanged();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void clearAll(){
+        if(toppingsList!=null){
+            toppingsList.clear();
+
+            if(toppingRecylerAdapter !=null){
+                toppingRecylerAdapter.notifyDataSetChanged();
+            }
+
+        }
+        toppingsList= new ArrayList<>();
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/logindatabase/ui/topping/ToppingRecylerAdapter.java
@@ -0,0 +1,218 @@
+package com.example.logindatabase.ui.topping;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import com.example.logindatabase.R;
+import com.example.logindatabase.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ToppingRecylerAdapter extends RecyclerView.Adapter<ToppingRecylerAdapter.ViewHolder> {
+
+    private static final String tag = "RecylerView";
+    private Context mContext;
+    private ArrayList<Toppings> toppingList;
+    private DatabaseReference myRef, numRef;
+
+    private Context getmContext;
+    //get user detail
+    //get User detail
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+    //child replace "1"
+    private String userName="";
+
+    public ToppingRecylerAdapter(Context mContext, ArrayList<Toppings> toppingList) {
+        this.mContext = mContext;
+        this.toppingList = toppingList;
+    }
+
+
+    @NonNull
+    @Override
+    public ToppingRecylerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //hold the view and put it on this template
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.topping_item, parent, false);
+
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //here is comment to click change layout and database
+
+        String currentTopping = toppingList.get(position).getToppingTitle();
+        final String[] numhelper = {""};
+
+        //show the values
+        holder.toppingName.setText(toppingList.get(position).getToppingName());
+        holder.toppingPrice.setText(toppingList.get(position).getToppingPrice());
+//        holder.toppingNum.setText(toppingList.get(position).getToppingNum());
+        holder.toppingTitle.setText(toppingList.get(position).getToppingTitle());
+
+        String checkNum=toppingList.get(position).getToppingNum();
+        if(checkNum.equals("0")){
+            holder.toppingNum.setText("We are preparing");
+        }else{
+            holder.toppingNum.setText("Ready to order");
+        }
+
+        //get user detail
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID=user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile !=null){
+                    String fullName=userProfile.fullName;
+                    userName=fullName;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+//                Toast.makeText(getClass().this,"Something wrong on our app",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        //image:Glide Library
+        Glide.with(mContext)
+                .load(toppingList.get(position).getToppingImage())
+                .into(holder.toppingImage);
+
+        //change to the child("username")
+        numRef = FirebaseDatabase.getInstance().getReference()
+                .child("Cart").child(userName);
+        numRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.hasChild(currentTopping)) {
+                    numhelper[0] ="0";
+                    return;
+                }else{
+//                    numRef.child(currentTopping).addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                            Map<String,Object> map=(Map<String, Object>) snapshot.getValue();
+//                            Object number=map.get("cartProductNum");
+//                            String num=String.valueOf(number);
+//                            numhelper=num;
+//                            return;
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError error) {
+//
+//                        }
+//                    });
+                    //new method
+                    for(DataSnapshot ds: snapshot.getChildren()){
+                        Map<String,Object> map=(Map<String, Object>) ds.getValue();
+                        Object title=map.get("cartProductTitle");
+                        String currentTitle=String.valueOf(title);
+                        if(currentTitle.equals(currentTopping)){
+                            Object number=map.get("cartProductNum");
+                            String num=String.valueOf(number);
+                            numhelper[0] =num;
+                            //stop the for loop
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //bug on numberHelper //itemview.set
+        if(!checkNum.equals("0")) {
+            holder.toppingImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //get intent
+                    Intent intent = new Intent(v.getContext(), ToppingViewActivity.class);
+                    intent.putExtra("productKey", currentTopping);
+                    intent.putExtra("productNum", numhelper[0]);
+                    v.getContext().startActivities(new Intent[]{intent});
+                }
+            });
+        }
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return toppingList.size();
+    }
+
+    public void filteredList(ArrayList<Toppings> filterList) {
+        toppingList=filterList;
+        notifyDataSetChanged();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        //widget
+        ImageView toppingImage;
+        TextView toppingName;
+        TextView toppingPrice;
+
+        //btn
+        private Button toppingAdd, toppingDrop;
+        TextView toppingNum;
+        TextView toppingTitle;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            toppingImage = itemView.findViewById(R.id.toppingImage);
+            toppingName = itemView.findViewById(R.id.toppingName);
+            toppingPrice = itemView.findViewById(R.id.toppingPrice);
+
+            toppingNum = itemView.findViewById(R.id.toppingOrderNum);
+//            toppingAdd = itemView.findViewById(R.id.toppingShopAdd);
+//            toppingDrop = itemView.findViewById(R.id.toppingShopDelete);
+
+            toppingTitle = itemView.findViewById(R.id.toppingTitle);
+
+        }
+    }
+
+}
